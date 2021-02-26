187d4bf7d3466c76162a2724b293c8357b38d83f
Hp <Hp@DESKTOP-SDJ197K>
2021-02-26 23:21:34 +0630

    TownshipRepository

---
+++ NHM-WEB/src/main/java/com/rbx/nhm/web/repositories/TownshipRepository.java
@@ -0,0 +1,10 @@
+package com.rbx.nhm.web.repositories;
+
+import com.rbx.nhm.web.entities.Township;
+
+public class TownshipRepository extends MainRepository<Township> {
+
+	public TownshipRepository() {
+		super(Township.class);
+		}
+}
